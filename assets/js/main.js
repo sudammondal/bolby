0311d104c5506e88ce2ff6278bf16af6d5c51fc4
Sudam Mondal <64835411+sudammondal@users.noreply.github.com>
2020-12-08 17:27:17 +0600

    add scrollspy

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
+    // sidebar manu  js //
+    function w3_open() {
+        document.getElementById("main").style.marginLeft = "25%";
+        document.getElementById("mySidebar").style.width = "25%";
+        document.getElementById("mySidebar").style.display = "block";
+        document.getElementById("openNav").style.display = 'none';
+    }
+
+    function w3_close() {
+        document.getElementById("main").style.marginLeft = "0%";
+        document.getElementById("mySidebar").style.display = "none";
+        document.getElementById("openNav").style.display = "inline-block";
+    }
 
     // counting ja //
     jQuery(document).ready(function($) {
