115390d870c9c16321778adab60d8a4eff1942a9
Sudam Mondal <64835411+sudammondal@users.noreply.github.com>
2020-12-10 17:44:09 +0600

    responsive add coplited

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,4 +94,18 @@ $(document).ready(function() {
     }
     isoTopActive();
     // ========== filtering========== //
+    $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false
+    });
+    var soundName;
+    var audioElement;
+
+    // MagnificPopup  
+
 });
